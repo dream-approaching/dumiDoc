e3385188ee213b4a28a3184ed47cca292bd21383
longzi <891661163@qq.com>
2020-05-28 12:59:38 +0800

    修改publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   title: 'Longzi Doc',
   hash: true, // 文件hash后缀
   history: { type: 'hash' },
+  publicPath: '/dumiDoc/',
   // more config: https://d.umijs.org/config
 });
