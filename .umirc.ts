419ad12ab74601a61e0c2b6f8ac3b254825f5673
longzi <891661163@qq.com>
2020-06-08 18:21:37 +0800

    统计

--- .umirc.ts
+++ .umirc.ts
@@ -5,5 +5,7 @@ export default defineConfig({
   hash: true, // 文件hash后缀
   history: { type: 'hash' },
   publicPath: '/dumiDoc/',
+  scripts: ['https://v1.cnzz.com/z_stat.php?id=1278974410&web_id=1278974410'],
+  styles: ['a[title=站长统计] { display: none; }'],
   // more config: https://d.umijs.org/config
 });
